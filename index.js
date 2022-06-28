011ebf101b9860bb5e6859b0ff31ae8b9180e98e
ayanbajo <bolatitoayanbajo@gmail.com>
2022-06-27 21:46:38 -0400

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function newImage(src, left, bottom){
     let image = document.createElement('img')
     image.src = src
     image.style.position = 'fixed'
-    image.style.left = left + 'px'
+    image.style.left =left + 'px'
     image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
@@ -39,34 +39,20 @@ newImage('assets/green-character.gif', 100, 100)
 // sword.left = '500px'
 // sword.bottom = '405px'
 // document.body.append(sword)
-
 // sword.addEventListener('click', function(){
 //     sword.remove()
 // })
-
-// function newItem(src, left, bottom){
-//     let newItem = document.createElement('img')
-// newItem.src = src
-// newItem.position = 'fixed'
-// newItem.left = left + 'px'
-// newItem.bottom = bottom + 'px'
-// document.body.append(newItem)
-
-// newItem.addEventListener('click', function(){
-//     newItem.remove()
-// })
 // }
-
 
 function newItem(src, left, bottom){
     let image = newImage(src, left, bottom)
-
-    image.addEventListener('dblclick', () => {
-        image.remove()
-    })
+    image.addEventListener('dblclick', function(){
+    image.remove()
+})
 }
 
 
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
